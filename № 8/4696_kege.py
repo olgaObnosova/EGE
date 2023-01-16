4ebd12b7932bbaf461d004611cdc4d5884c3e856
teacher <obnolga@mail.ru>
2023-01-16 17:29:04 +0300

    bbb

--- № 8/4696_kege.py
+++ № 8/4696_kege.py
@@ -6,6 +6,7 @@ for x in a:
     if x[0]!='0' and '16' not in x and '61' not in x\
         and '36' not in x and '63' not in x and \
             '56' not in x and '65' not in x and \
+             '76' not in x and '67' not in x and\
             x.count('6') == 1:
         k += 1
 print(k)
---
+++ №19-21/4707_kege.py
@@ -0,0 +1,14 @@
+def f(s, h, max_h):
+    if s > 128:
+        return h % 2 == max_h % 2
+    if h == max_h:
+        return 0
+    if h % 2 != max_h:
+        return f(s + 1, h + 1, max_h) or f(s * 2, h + 1, max_h)
+    else:
+        return f(s + 1, h + 1, max_h) and f(s * 2, h + 1, max_h)
+for x in range(1, 129):
+    for j in range(1,10):
+        if f(x,0,j):
+            print(x, j)
+            break
