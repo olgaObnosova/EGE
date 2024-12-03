65d0b9a6e5dc94e38e49d0d50cb62386606e12a8
Кузнецова Ольга Анатольевна <obnolga@mail.ru>
2024-12-03 11:53:23 +0300

    sss

--- ИТ марафон/8_1_2024.py
+++ ИТ марафон/8_1_2024.py
@@ -1,8 +1,11 @@
-a=input()
-a=a+a[0]
-a=a[::-1]
-sp='OIZE!SbJ#P'
-n=''
-for x in a:
-    n+=sp[int(x)]
-print(n)
+a=str(int(input()))
+if len(a)==7:
+    a=a+a[0]
+    a=a[::-1]
+    sp='OIZE!SbJ#P'
+    n=''
+    for x in a:
+        n+=sp[int(x)]
+    print(n)
+else:
+    print('WRONG')
